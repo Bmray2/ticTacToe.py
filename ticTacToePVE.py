7e72ed29eca01f0a6857440d69823c5acd7472ee
Bmray2 <41490544+Bmray2@users.noreply.github.com>
2024-05-13 20:09:19 -0400

    Add files via upload
    Added number generator and option to vs the computer as a pve experience

---
+++ ticTacToePVE.py
@@ -0,0 +1,224 @@
+import sys
+import random
+
+# Method used to print and draw the board as the game is played.
+# theBoard: Dictionary containing the current occupying player for each square
+def displayBoard(theBoard):
+
+    print('  |--------------------|')
+    print('  |      |      |      |')
+    print('3 |  '+ theBoard['a3'] +'   |   ' + theBoard['b3'] + '  |   ' + theBoard['c3'] + '  |')
+    print('  |      |      |      |')
+    print('  |------+------+------|')
+    print('  |      |      |      |')
+    print('2 |  '+ theBoard['a2'] +'   |   ' + theBoard['b2'] + '  |   ' + theBoard['c2'] + '  |')
+    print('  |      |      |      |')
+    print('  |------+------+------|')
+    print('  |      |      |      |')
+    print('1 |  '+ theBoard['a1'] +'   |   ' + theBoard['b1'] + '  |   ' + theBoard['c1'] + '  |')
+    print('  |      |      |      |')
+    print('  |--------------------|')
+    print('     A       B     C   ')
+    print()
+
+# Method that takes the user inpute to verify it is a valid move or breaks the rules of the game.
+# theBoard: Dictionary containing the current occupying player for each square
+# player: variable indicating which players turn it is
+def validMove(theBoard, player):
+
+    valid = False
+
+    while valid == False:
+        print()
+        playerMove = input('It is ' + player + '\'s turn. Please enter the coordinate for your turn.')
+
+        if playerMove.lower() == 'quit' or playerMove.lower() == 'q' :
+            sys.exit("Thank you for playing.")
+        elif playerMove.lower() == 'a3' or playerMove.lower() == 'b3' or playerMove.lower() == 'c3' or playerMove.lower() == 'a2' or playerMove.lower() == 'b2'or playerMove.lower() == 'c2' or playerMove.lower() == 'a1' or playerMove.lower() == 'b1' or playerMove.lower() == 'c1':
+            if theBoard[playerMove.lower()] != ' ':
+                print('This space is already claimed. Please select a space that is currently not claimed')
+            else:
+                print('Player ' + player + ' has claimed the space ' + playerMove.upper() + '.')
+                theBoard[playerMove.lower()] = player
+                valid = True
+        else:
+            print('The coordinate entered does not match a space on the board. Please enter a valid space.')
+    return theBoard, playerMove.lower()
+
+# Checks most recent move by the player to verify if the move has won the game or not
+# theBoard: Dictionary containing the current occupying player for each square
+# player: variable indicating which players turn it is
+def checkWinner(theBoard, player):
+
+    if theBoard['a3'] == theBoard['a2'] == theBoard['a1'] != ' ':
+        return True
+    elif theBoard['b3'] == theBoard['b2'] == theBoard['b1'] != ' ':
+        return True
+    elif theBoard['c3'] == theBoard['c2'] == theBoard['c1'] != ' ':
+        return True
+    elif theBoard['a3'] == theBoard['b3']  == theBoard['c3'] != ' ':
+        return True
+    elif theBoard['a2'] == theBoard['b2'] == theBoard['c2'] != ' ':
+        return True
+    elif theBoard['a1'] == theBoard['b1']  == theBoard['c1'] != ' ':
+        return True
+    elif theBoard['a1'] == theBoard['b2'] == theBoard['c3'] != ' ':
+        return True
+    elif theBoard['a3'] == theBoard['b2'] == theBoard['c1'] != ' ':
+        return True
+    else:
+        return False
+
+def pveGame():
+
+    turn = 1
+    player = 'X'
+    winsX = 0
+    winsO = 0
+    numTies = 0
+    theBoard = {'a3': ' ', 'b3': ' ', 'c3': ' ',
+                'a2': ' ', 'b2': ' ', 'c2': ' ',
+                'a1': ' ', 'b1': ' ', 'c1': ' '}
+
+    displayBoard(theBoard)
+
+    while turn < 10:
+        turn = 1
+        player = 'X'
+        theBoard = {'a3': ' ', 'b3': ' ', 'c3': ' ',
+                    'a2': ' ', 'b2': ' ', 'c2': ' ',
+                    'a1': ' ', 'b1': ' ', 'c1': ' '}
+
+        print('The board is broken down into coordinates such as \"A1, B2, C3\". Please enter move in this format to claim an unoccupied space on your turn.')
+        print('At any point entering \"quit\" or \"q\" during the players move will close the session')
+
+        availableSpaces = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
+
+        
+        for i in range(9):
+            if player == 'X':
+                theBoard, playerMove = validMove(theBoard, player)
+                availableSpaces.remove(playerMove)
+                print()
+            else:
+                playerMove = random.choice(availableSpaces)
+                theBoard[playerMove.lower()] = player
+                availableSpaces.remove(playerMove)
+                print()
+                 
+            print('Turn ' + str(turn))
+            displayBoard(theBoard)
+
+            if turn >= 5:
+
+                if checkWinner(theBoard, player) == True and player == 'X':
+                    print('Player ' +player+ ' is the Winner.')
+                    winsX += 1
+                    print( 'Wins X : ' + str(winsX))
+                    print( 'Wins O : ' + str(winsO))
+                    print( 'Ties : ' + str(numTies))
+                    print()
+                    break
+                elif checkWinner(theBoard, player) == True and player == 'O':
+                    print('Player ' +player+ ' is the Winner.')
+                    winsO += 1
+                    print( 'Wins X : ' + str(winsX))
+                    print( 'Wins O : ' + str(winsO))
+                    print( 'Ties : ' + str(numTies))
+                    print()
+                    break
+                elif checkWinner(theBoard, player) == False and turn == 9:
+                    print('The round has ended in a Tie')
+                    numTies += 1
+                    print( 'Wins X : ' + str(winsX))
+                    print( 'Wins O : ' + str(winsO))
+                    print( 'Ties : ' + str(numTies))
+                    print()
+                    break
+
+            if player == 'X':
+                player = 'O'
+            else:
+                player = 'X'
+
+            turn += 1
+        
+
+def ticTacToe():
+
+    turn = 1
+    player = 'X'
+    winsX = 0
+    winsO = 0
+    numTies = 0
+    theBoard = {'a3': ' ', 'b3': ' ', 'c3': ' ',
+                'a2': ' ', 'b2': ' ', 'c2': ' ',
+                'a1': ' ', 'b1': ' ', 'c1': ' '}
+
+    displayBoard(theBoard)
+
+    while turn < 10:
+        turn = 1
+        player = 'X'
+        theBoard = {'a3': ' ', 'b3': ' ', 'c3': ' ',
+                    'a2': ' ', 'b2': ' ', 'c2': ' ',
+                    'a1': ' ', 'b1': ' ', 'c1': ' '}
+
+        print('The board is broken down into coordinates such as \"A1, B2, C3\". Please enter move in this format to claim an unoccupied space on your turn.')
+        print('At any point entering \"quit\" or \"q\" during the players move will close the session')
+        
+        for i in range(9):
+            theBoard = validMove(theBoard, player)[0]
+            print()
+            print('Turn ' + str(turn))
+            displayBoard(theBoard)
+
+            if turn >= 5:
+
+                if checkWinner(theBoard, player) == True and player == 'X':
+                    print('Player ' +player+ ' is the Winner.')
+                    winsX += 1
+                    print( 'Wins X : ' + str(winsX))
+                    print( 'Wins O : ' + str(winsO))
+                    print( 'Ties : ' + str(numTies))
+                    print()
+                    break
+                elif checkWinner(theBoard, player) == True and player == 'O':
+                    print('Player ' +player+ ' is the Winner.')
+                    winsO += 1
+                    print( 'Wins X : ' + str(winsX))
+                    print( 'Wins O : ' + str(winsO))
+                    print( 'Ties : ' + str(numTies))
+                    print()
+                    break
+                elif checkWinner(theBoard, player) == False and turn == 9:
+                    print('The round has ended in a Tie')
+                    numTies += 1
+                    print( 'Wins X : ' + str(winsX))
+                    print( 'Wins O : ' + str(winsO))
+                    print( 'Ties : ' + str(numTies))
+                    print()
+                    break 
+            
+            if player == 'X':
+                player = 'O'
+            else:
+                player = 'X'
+
+            turn += 1
+        
+if __name__ == "__main__":
+
+    validType = False
+    while validType != True:
+        gameType = input('Would you like a PVP or a PVE Game? ')
+        print()
+        if gameType.lower() ==  'pvp':
+            ticTacToe()
+            validType = True
+        elif gameType.lower() ==  'pve':
+            pveGame()
+            validType = True
+        else:
+            print('Invalid game type selected please try again.')
+            print()
